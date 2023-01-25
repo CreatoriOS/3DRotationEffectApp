bb883257aaedb496e0387f551dd12bfc39b37cfe
ZHANSAYA BORTANOVA <123213674+CreatoriOS@users.noreply.github.com>
2023-01-25 05:55:58 +0400

    Add files via upload

---
+++ 3drotationeffect/3drotationeffect/_drotationeffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  _drotationeffectApp.swift
+//  3drotationeffect
+//
+//  Created by Zhansaya Bortanova on 13/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct _drotationeffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
